227f37367fba137c8e778be932511e6f88b4dd02
skoliver89 <soliver14@wou.edu>
2018-03-09 12:36:08 -0800

    Create UP scripit; Script Create UserAccounts Table

---
+++ UP.sql
@@ -0,0 +1,10 @@
+CREATE TABLE dbo.UserAccounts
+(
+  UserID  NVARCHAR(50)  NOT NULL, /*Populated with Google SID On first login*/
+  Username  NVARCHAR(20)  NOT NULL,
+  Timezone  NVARCHAR(5) NULL,
+  TimeFormat  NVARCHAR(5) NULL,
+  Email NVARCHAR(50)  NOT NULL
+
+  CONSTRAINT[PK_dbo.UserAccounts] PRIMARY KEY (UserID)
+);
