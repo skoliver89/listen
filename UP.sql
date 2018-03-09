8eea6f414e68759a2c6a7046d1dcb3e906b95c63
skoliver89 <soliver14@wou.edu>
2018-03-09 12:47:38 -0800

    Force UserID, UserName, and Email to be Unique in UserAccounts table

--- UP.sql
+++ UP.sql
@@ -1,10 +1,12 @@
 CREATE TABLE dbo.UserAccounts
 (
-  UserID  NVARCHAR(50)  NOT NULL, /*Populated with Google SID On first login*/
-  Username  NVARCHAR(20)  NOT NULL,
+   /* Populated with Google SID On first login
+      Used as Primary Key for User Look-up */
+  UserID  NVARCHAR(50)  NOT NULL UNIQUE,
+  Username  NVARCHAR(20)  NOT NULL UNIQUE,
   Timezone  NVARCHAR(5) NULL,
   TimeFormat  NVARCHAR(5) NULL,
-  Email NVARCHAR(50)  NOT NULL
+  Email NVARCHAR(50)  NOT NULL UNIQUE
 
   CONSTRAINT[PK_dbo.UserAccounts] PRIMARY KEY (UserID)
 );
