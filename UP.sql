6f892ddd57bfe483495bae840c147eff49a8c0ed
skoliver89 <soliver14@wou.edu>
2018-03-09 12:57:12 -0800

    Fix errors in script and remove unnecessary email column

--- UP.sql
+++ UP.sql
@@ -5,8 +5,7 @@ CREATE TABLE dbo.UserAccounts
   UserID  NVARCHAR(50)  NOT NULL UNIQUE,
   Username  NVARCHAR(20)  NOT NULL UNIQUE,
   Timezone  NVARCHAR(5) NULL,
-  TimeFormat  NVARCHAR(5) NULL,
-  Email NVARCHAR(50)  NOT NULL UNIQUE
+  TimeFormat  NVARCHAR(5) NULL
 
   CONSTRAINT[PK_dbo.UserAccounts] PRIMARY KEY (UserID)
 );
