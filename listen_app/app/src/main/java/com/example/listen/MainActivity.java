cfced6912f635587c16672056d8c87817830c720
skoliver89 <soliver14@wou.edu>
2018-03-11 20:37:49 -0700

    Add Authentication through FireBase

--- listen_app/app/src/main/java/com/example/listen/MainActivity.java
+++ listen_app/app/src/main/java/com/example/listen/MainActivity.java
@@ -1,13 +1,57 @@
 package com.example.listen;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 123;
+
+    public void authenticate(){
+        //Create and launch sign-in intent
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .build(),
+                RC_SIGN_IN
+        );
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        authenticate();
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN){
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+
+            if (requestCode == RESULT_OK) {
+                //Successfully Sign in
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+                // ...
+            } else {
+                // Sign in failed, check response for error code
+                // ...
+
+            }
+        }
     }
 }
