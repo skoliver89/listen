ff3a8bb62c833b195e0b2d1a26b595afeaf3a4e4
skoliver89 <soliver14@wou.edu>
2018-03-11 23:10:39 -0700

    Add menu and sign out logic

--- listen_app/app/src/main/java/com/example/listen/MainActivity.java
+++ listen_app/app/src/main/java/com/example/listen/MainActivity.java
@@ -1,12 +1,19 @@
 package com.example.listen;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.TextView;
 
 import com.firebase.ui.auth.AuthUI;
 import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
@@ -17,14 +24,37 @@ public class MainActivity extends AppCompatActivity {
 
     private static final int RC_SIGN_IN = 123;
 
+// ...
+
+    // Choose authentication providers
+    @SuppressWarnings("deprecation")
+    List<AuthUI.IdpConfig> providers = Arrays.asList(
+            new AuthUI.IdpConfig.Builder(AuthUI.GOOGLE_PROVIDER).build()
+    );
+
     public void authenticate(){
-        //Create and launch sign-in intent
+        // Create and launch sign-in intent
         startActivityForResult(
                 AuthUI.getInstance()
                         .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
                         .build(),
-                RC_SIGN_IN
-        );
+                RC_SIGN_IN);
+    }
+
+    public void signOut(){
+        AuthUI.getInstance()
+                .signOut(this)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        // After sign out restart the app -> login prompt
+                        Intent i = getBaseContext().getPackageManager()
+                                .getLaunchIntentForPackage( getBaseContext().getPackageName() );
+                        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                        startActivity(i);
+                    }
+                });
     }
 
     @Override
@@ -36,22 +66,46 @@ public class MainActivity extends AppCompatActivity {
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data){
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-
-        if (requestCode == RC_SIGN_IN){
+        if (requestCode == RC_SIGN_IN) {
             IdpResponse response = IdpResponse.fromResultIntent(data);
 
-            if (requestCode == RESULT_OK) {
-                //Successfully Sign in
+            if (resultCode == RESULT_OK) {
+                // Successfully signed in
                 FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
-                // ...
+                TextView testText = findViewById(R.id.test_text);
+                String userName = user.getDisplayName();
+                testText.setText("Hello " + userName + "!");
+                testText.setVisibility(View.VISIBLE);
             } else {
                 // Sign in failed, check response for error code
                 // ...
-
             }
         }
     }
+
+    // ### MENU STUFF ###
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle item selection
+        switch (item.getItemId()) {
+            case R.id.menu_account:
+
+                return true;
+            case R.id.menu_sign_out:
+                signOut();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
 }
