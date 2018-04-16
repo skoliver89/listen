6a2ee2b222868bf51bf0e20a82e316fdb8f97131
skoliver89 <soliver14@wou.edu>
2018-04-16 12:19:48 -0700

    Create the Profile Activity

---
+++ listen_app/app/src/main/java/com/example/listen/ProfileActivity.java
@@ -0,0 +1,17 @@
+package com.example.listen;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ProfileActivity extends AppCompatActivity {
+    // ### Class Variables
+
+    // ### Custom Methods
+
+    // ### Overrides
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
