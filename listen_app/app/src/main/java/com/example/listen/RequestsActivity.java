769a5dde1633fcf5b40a8c6e6234a6815f0c10e1
skoliver89 <soliver14@wou.edu>
2018-05-27 20:07:57 -0700

    Set up the accept/deny dialog for requests

--- listen_app/app/src/main/java/com/example/listen/RequestsActivity.java
+++ listen_app/app/src/main/java/com/example/listen/RequestsActivity.java
@@ -1,8 +1,8 @@
 package com.example.listen;
 
+import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
--- listen_app/app/src/main/java/com/example/listen/RequestsListAdapter.java
+++ listen_app/app/src/main/java/com/example/listen/RequestsListAdapter.java
@@ -1,6 +1,8 @@
 package com.example.listen;
 
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,14 +12,51 @@ import android.widget.TextView;
 import java.util.List;
 
 public class RequestsListAdapter extends RecyclerView.Adapter<RequestsListAdapter.ViewHolder> {
+    // ### Class Variables
     public List<Friend> requestList;
     private Context mContext;
 
+    // ### Constructors
     public RequestsListAdapter(Context mContext, List<Friend> requestList) {
         this.requestList = requestList;
         this.mContext = mContext;
     }
 
+    // ### Custom Methods
+    private void requestAction(String uid, String alias){
+        // Create the Dialog for accepting or denying a request
+        AlertDialog.Builder requestDialog = new AlertDialog
+                .Builder(mContext);
+
+        // Set the Dialog Title
+        requestDialog.setTitle("Friend Request");
+
+        // Set the Dialog Message
+        String msg = alias + " would like to be your friend.";
+        requestDialog.setMessage(msg);
+
+        // Dialog Button Behaviors
+        // Accept Button
+        requestDialog.setPositiveButton("Accept", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        // Deny Button
+        requestDialog.setNegativeButton("Deny", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+
+        // Show the dialog
+        requestDialog.show();
+    }
+
+    // ### Overrides
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType){
         View view = LayoutInflater.from(parent.getContext())
@@ -27,7 +66,7 @@ public class RequestsListAdapter extends RecyclerView.Adapter<RequestsListAdapte
     }
 
     @Override
-    public void onBindViewHolder(ViewHolder holder, final int position){
+    public void onBindViewHolder(final ViewHolder holder, final int position){
         holder.aliasText.setText(requestList.get(position).getAlias());
 
         //Make the items clickable
@@ -35,6 +74,9 @@ public class RequestsListAdapter extends RecyclerView.Adapter<RequestsListAdapte
             @Override
             public void onClick(View v) {
                 //TODO Dialog to accept or deny friend requests
+                String uid = requestList.get(position).getUid();
+                String alias = requestList.get(position).getAlias();
+                requestAction(uid, alias);
             }
         });
     }
